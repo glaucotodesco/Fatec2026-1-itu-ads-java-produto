d41fe5fdec2510adff8d99a6f6ba1c3695eec9c5
glaucotodesco <glauco.todesco@gmail.com>
2026-05-19 16:19:11 -0300

    aula 19/05

--- src/main/java/com/fatec/product/services/ProductService.java
+++ src/main/java/com/fatec/product/services/ProductService.java
@@ -21,9 +21,42 @@ public class ProductService {
         return repository.findAll();
     }
 
-     public Product findById(Long id) {
+    public Product findById(Long id) {
         return repository.findById(id)
-                         .orElseThrow(() -> new EntityNotFoundException());
+                         .orElseThrow(() -> new EntityNotFoundException("Produto não cadastro"));
     }
+
+
+    public void deleteById(Long id)
+    {
+        if(repository.existsById(id))
+            repository.deleteById(id);
+        else
+           throw new EntityNotFoundException("Produto não cadastrado");
+    }
+
+    public Product save(Product product)
+    {
+         return repository.save(product);
+    }
+
+    public void update(Product product, Long id)
+    {
+        Product p  = repository.findById(id)
+                               .orElseThrow(() -> new EntityNotFoundException("Produto não cadastrado"));
+
+        p.setDescription(product.getDescription());                                
+        p.setName(product.getName());
+        p.setPrice(product.getPrice());
+
+        repository.save(p);
+
+
+    }
+
+    
+
+
+
     
 }
